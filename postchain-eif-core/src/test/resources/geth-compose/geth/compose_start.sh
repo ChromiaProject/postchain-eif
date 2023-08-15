38fa84374dc7100e8c06ac9fe78d76518f2aa9b4
Ha ĐANG <dvietha@gmail.com>
2023-08-15 15:24:13 +0700

    using geth dev mode for post-merge

--- postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
+++ postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-geth --nousb init /geth/test.json
-geth --nousb --password /geth/password.txt account import /geth/key.txt
-geth --nousb --http --http.addr=0.0.0.0 --http.vhosts=* --allow-insecure-unlock --unlock 0x659e4a3726275edfd125f52338ece0d54d15bd99 --password /geth/password.txt --mine --rpc.allow-unprotected-txs
+geth --networkid=1 --nousb --http --http.addr=0.0.0.0 --dev --dev.period=1 --rpc.allow-unprotected-txs
