0cf5d07eb83f592c76c65950bd69155b94ed9970
Ha ĐANG <dvietha@gmail.com>
2023-08-16 10:42:33 +0700

    use customize geth client for testing

--- postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
+++ postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --networkid=1 --nousb --http --http.addr=0.0.0.0 --dev --dev.period=1 --rpc.allow-unprotected-txs
+geth --networkid=1 --nousb --http --http.addr=0.0.0.0 --http.vhosts=* --dev --dev.period=1 --rpc.allow-unprotected-txs
