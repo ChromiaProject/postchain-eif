869ca720c04fa2c24728de86848d4b45ec23cc27
Ha ĐANG <dvietha@gmail.com>
2023-08-23 09:13:47 +0700

    use geth dev mode for testing

--- postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
+++ postchain-eif-core/src/test/resources/geth-compose/geth/compose_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-geth --nousb init /geth/test.json
-geth --nousb account import /geth/key.txt --password /geth/password.txt
-geth --nousb --http --http.addr=0.0.0.0 --http.vhosts=* --allow-insecure-unlock --unlock 0xe105ba42b66d08ac7ca7fc48c583599044a6dab3 --password /geth/password.txt --mine --rpc.allow-unprotected-txs
+geth --nousb \
+    --http --http.addr=0.0.0.0 --http.vhosts=* \
+    --dev --dev.period=1 --rpc.allow-unprotected-txs
