ac444896fe5ee116f541ca4617555f0265a142ac
Ha ĐANG <dvietha@gmail.com>
2023-12-19 13:30:25 +0700

    remove sample ui to other place

--- postchain-eif-ui/src/util.ts
+++
@@ -1,9 +0,0 @@
-import { Flag, HexString } from "./types";
-
-export const createAuthDesc = (pubkey: HexString, flags: Flag[] = [Flag.TRANSFER]) => {
-    return [
-        0,
-        [flags, pubkey],
-        null,
-    ]
-}
