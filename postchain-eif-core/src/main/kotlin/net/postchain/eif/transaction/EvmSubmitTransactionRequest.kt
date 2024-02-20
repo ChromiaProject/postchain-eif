7dedea9331a56058876185c981e8d5f5e700dc17
Robert Wideberg <robert.wideberg@chromaway.com>
2024-02-20 15:30:57 +0100

    Bumping postchain version to 3.15.0

--- postchain-eif-core/src/main/kotlin/net/postchain/eif/transaction/EvmSubmitTransactionRequest.kt
+++ postchain-eif-core/src/main/kotlin/net/postchain/eif/transaction/EvmSubmitTransactionRequest.kt
@@ -13,7 +13,7 @@ data class EvmSubmitTransactionRequest(
         @Name("parameter_types")
         val parameterTypes: List<String>,
         @Name("parameter_values")
-        val parameterValues: Gtv, // TODO change to List<Gtv> when bug is fixed
+        val parameterValues: List<Gtv>,
         @Name("network_id")
         val networkId: Long,
         @Name("sender")
