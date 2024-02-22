bbe91319f18e11d2249fa2898650bdf74f36488e
Robert Wideberg <robert.wideberg@chromaway.com>
2024-02-22 09:54:01 +0000

    POS-1109: Extract messaging format to separate module so that it can be...

--- postchain-eif-core/src/main/kotlin/net/postchain/eif/transaction/config/EvmTransactionSubmitterBlockchainConfig.kt
+++
@@ -1,8 +0,0 @@
-package net.postchain.eif.transaction.config
-
-import net.postchain.gtv.mapper.Name
-
-data class EvmTransactionSubmitterBlockchainConfig(
-        @Name("network_id")
-        val networkId: Long
-)
