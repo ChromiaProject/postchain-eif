208c5ade4f82c2571aacf51e249154b822be94c0
Koray Kaya <koray.kaya@chromaway.com>
2024-03-19 17:22:58 +0400

    Change IERC20 to ChromiaToken

--- postchain-eif-contracts/contracts/utils/test/TokenBridgeDelegator.sol
+++ postchain-eif-contracts/contracts/utils/test/TokenBridgeDelegator.sol
@@ -7,18 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../../TokenBridge.sol";
 
 contract TokenBridgeDelegator {
-
     TokenBridge private _bridge;
 
     constructor(TokenBridge bridge) {
         _bridge = bridge;
     }
 
-    function approve(IERC20 token, address _spender, uint256 _amount) public returns(bool) {
+    function approve(IERC20 token, address _spender, uint256 _amount) public returns (bool) {
         return token.approve(_spender, _amount);
     }
 
-    function deposit(IERC20 token, uint256 amount) public returns (bool) {
+    function deposit(ChromiaToken token, uint256 amount) public returns (bool) {
         return _bridge.deposit(token, amount);
     }
 
