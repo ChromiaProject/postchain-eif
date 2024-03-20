357fd8a210f60f1b1da6f4d5c0b75764fee16ba0
Robert Wideberg <robert.wideberg@chromaway.com>
2024-03-20 12:25:58 +0100

    Configuration fixes for tx submitter

--- postchain-eif-core/src/main/kotlin/net/postchain/eif/transaction/config/TransactionSubmitterBlockchainConfig.kt
+++ postchain-eif-core/src/main/kotlin/net/postchain/eif/transaction/config/TransactionSubmitterBlockchainConfig.kt
@@ -1,5 +1,6 @@
 package net.postchain.eif.transaction.config
 
+import net.postchain.gtv.mapper.DefaultValue
 import net.postchain.gtv.mapper.Name
 import net.postchain.gtv.mapper.Nullable
 
@@ -9,14 +10,19 @@ data class TransactionSubmitterBlockchainConfig(
         @Name("gas_limit")
         val gasLimit: Long,
         @Name("node_tx_timeout")                        // Timeout for submitting TX
+        @DefaultValue(86400000)
         val nodeTxTimeout: Long,
         @Name("node_tx_verification_timeout")           // Timeout for verifying TX
+        @DefaultValue(240000)
         val nodeTxVerificationTimeout: Long,
         @Name("node_tx_verification_evm_blocks")        // Number of EVM blocks to await before verification
+        @DefaultValue(5)
         val nodeTxVerificationEvmBlocks: Long,
         @Name("tx_verification_time")                   // Elapsed time before verification consensus begins
+        @DefaultValue(300000)
         val txVerificationTime: Long,
         @Name("db_retention_time")                        // Time to keep data in db
+        @DefaultValue(1296000000)
         val dbRetentionTime: Long,
         @Name("system_anchoring_brid")
         @Nullable
