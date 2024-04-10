71e78536268ba4b451616441ce77e7bda6976d7e
Koray Kaya <koray.kaya@chromaway.com>
2024-04-10 14:31:17 +0100

    added basic two week delay

--- postchain-eif-contracts/contracts/DailyLimit.sol
+++ postchain-eif-contracts/contracts/DailyLimit.sol
@@ -4,14 +4,19 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DailyLimit is Ownable {
-    uint dayStart; // Timestamp at which the day started
-    uint dayAmount; // Amount of tokens withdrawn so far
-    uint dayLimit; // Maximum amount of tokens that can be withdrawn in a day
+    uint private dayStart; // Timestamp at which the day started
+    uint private dayAmount; // Amount of tokens withdrawn so far
+    uint private dayLimit; // Maximum amount of tokens that can be withdrawn in a day
+    uint private pendingDayLimit; // New day limit pending to be activated after 2 weeks
+    uint private limitIncreaseRequestTime; // Timestamp when the limit increase was requested
+
+    uint public constant LIMIT_INCREASE_DELAY = 2 weeks;
 
     address public parentContract;
 
     event DayLimitChanged(uint newDayLimit);
     event ParentContractChanged(address newParentContract);
+    event LimitIncreaseRequested(uint newPendingDayLimit, uint requestTime);
 
     constructor(uint _dayLimit) Ownable(msg.sender) {
         dayStart = block.timestamp;
@@ -19,9 +24,34 @@ contract DailyLimit is Ownable {
         dayLimit = _dayLimit;
     }
 
-    function setDayLimit(uint newDayLimit) external onlyOwner {
-        dayLimit = newDayLimit;
-        emit DayLimitChanged(newDayLimit);
+    // Function to modify the day limit
+    function setDayLimit(uint _newDayLimit) external onlyOwner {
+        if (_newDayLimit <= dayLimit) {
+            // If the new limit is lower, apply immediately
+            dayLimit = _newDayLimit;
+            emit DayLimitChanged(_newDayLimit);
+        } else {
+            // If the new limit is higher, start the 2-week waiting period
+            pendingDayLimit = _newDayLimit;
+            limitIncreaseRequestTime = block.timestamp;
+            emit LimitIncreaseRequested(_newDayLimit, block.timestamp);
+        }
+    }
+
+    // Function to apply the pending limit increase after 2 weeks
+    function applyPendingLimitIncrease() external onlyOwner {
+        require(
+            block.timestamp >= limitIncreaseRequestTime + LIMIT_INCREASE_DELAY,
+            "DailyLimit: Waiting period has not passed."
+        );
+        require(pendingDayLimit > dayLimit, "DailyLimit: No pending limit increase or not higher than limit.");
+
+        dayLimit = pendingDayLimit;
+        // Reset the pending limit and request time
+        pendingDayLimit = 0;
+        limitIncreaseRequestTime = 0;
+
+        emit DayLimitChanged(dayLimit);
     }
 
     function setParentContract(address _parentContract) external onlyOwner {
