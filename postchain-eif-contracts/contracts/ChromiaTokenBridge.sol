3e8fc7b608a59685f8aeca97a24a1514ba7ed060
Robert Wideberg <robert.wideberg@chromaway.com>
2024-04-16 12:26:02 +0200

    Snapshot withdraw removal + refactoring to reduce code duplication

--- postchain-eif-contracts/contracts/ChromiaTokenBridge.sol
+++ postchain-eif-contracts/contracts/ChromiaTokenBridge.sol
@@ -36,69 +36,12 @@ contract ChromiaTokenBridge is TokenBridge {
         tokenMinter = _tokenMinter;
     }
 
-    function deposit(IERC20 token, uint256 amount) public override isAllowToken(token) whenNotPaused returns (bool) {
-        (string memory name, string memory symbol, uint8 decimals) = _getTokenInfo(token);
+    function transferDeposit(IERC20 token, uint256 amount) internal override {
         token.safeTransferFrom(msg.sender, address(tokenMinter), amount);
         tokenMinter.burn(amount);
-        emit DepositedERC20(msg.sender, token, networkId, amount, name, symbol, decimals);
-        return true;
     }
 
-    function withdraw(bytes32 _hash, address payable beneficiary) external override whenNotPaused nonReentrant {
-        Withdraw storage wd = _withdraw[_hash];
-        require(wd.beneficiary == beneficiary, "TokenBridge: no fund for the beneficiary");
-        require(wd.block_number <= block.number, "TokenBridge: not mature enough to withdraw the fund");
-        require(wd.status == Status.Withdrawable, "TokenBridge: fund is pending or was already claimed");
-        wd.status = Status.Withdrawn;
-        uint value = wd.amount;
-        wd.amount = 0;
-        // only support user to withdraw the token that be funded enough on the EVM bridge
+    function transferWithdraw(IERC20 token, address beneficiary, uint value) internal override {
         tokenMinter.mint(beneficiary, value);
-        emit Withdrawal(beneficiary, wd.token, value);
-    }
-
-    /**
-     * @dev withdraw all account assets in the postchain snapshot when mass exit was triggered
-     * Note: the mass exit block should be the block at which snapshot was updated
-     *          with state root was stored properly in the block header extra data.
-     */
-    function withdrawBySnapshot(
-        bytes calldata snapshot,
-        Data.Proof memory stateProof,
-        bytes memory blockHeader,
-        bytes[] memory sigs,
-        address[] memory signers,
-        Data.ExtraProofData memory extraProof
-    ) public override whenMassExit whenNotPaused nonReentrant {
-        require(_snapshots[stateProof.leaf] == false, "TokenBridge: snapshot already used");
-        require(stateProof.leaf == keccak256(snapshot), "TokenBridge: snapshot data is not correct");
-        require(Hash.hashGtvBytes64Leaf(extraProof.leaf) == extraProof.hashedLeaf, "Postchain: invalid EIF extra data");
-        (uint height, bytes32 blockRid) = Postchain.verifyBlockHeader(blockchainRid, blockHeader, extraProof);
-        bytes32 stateRoot = _bytesToBytes32(extraProof.leaf, 32);
-        require(
-            blockRid == massExitBlock.blockRid && height == massExitBlock.height,
-            "TokenBridge: snapshot block should be the same with mass exit block"
-        );
-        if (!validator.isValidSignatures(blockRid, sigs, signers)) revert("TokenBridge: block signature is invalid");
-        if (!MerkleProof.verify(stateProof.merkleProofs, stateProof.leaf, stateProof.position, stateRoot))
-            revert("TokenBridge: invalid merkle proof");
-
-        address beneficiary = abi.decode(snapshot[:32], (address));
-        uint offset = 32;
-        // Get byte size of all ERC20 balances
-        uint byteSize = abi.decode(snapshot[offset:offset + 32], (uint));
-        offset += 32;
-        for (uint i = offset; i < offset + byteSize; i += ERC20_ACCOUNT_STATE_BYTE_SIZE) {
-            ERC20AccountState memory accountState = abi.decode(
-                snapshot[i:i + ERC20_ACCOUNT_STATE_BYTE_SIZE],
-                (ERC20AccountState)
-            );
-            if (accountState.amount > 0 && _allowedToken[accountState.token]) {
-                tokenMinter.mint(beneficiary, accountState.amount);
-            }
-        }
-
-        _snapshots[stateProof.leaf] = true;
-        emit WithdrawalBySnapshot(beneficiary);
     }
 }
