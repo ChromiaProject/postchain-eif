e2477a3fe652bd3a0f8ee1054311fff2cc1032dd
Eugene Tykulov <eugene.tykulov@chromaway.com>
2025-05-16 19:17:48 +0300

    Fix the script #2

--- postchain-eif-contracts/tasks/deployers/bridge.ts
+++ postchain-eif-contracts/tasks/deployers/bridge.ts
@@ -187,8 +187,7 @@ task("approveToken:bridge")
     .addParam("tokenAddress", "Token contract address")
     .addParam("amount", "Amount of tokens to approve")
     .setAction(async ({ bridgeAddress, tokenAddress, amount }, hre) => {
-        const tokenFactory = await hre.ethers.getContractFactory("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20") as IERC20__factory;
-        const token = tokenFactory.attach(tokenAddress) as IERC20;
+        const token = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", tokenAddress) as IERC20;
 
         console.log(`Approving bridge ${bridgeAddress} to spend ${amount} tokens from ${tokenAddress}...`);
         const tx = await token.approve(bridgeAddress, amount);
