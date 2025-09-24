629a2b9ef3fab5c4c6e33b656a4b6618adf29618
alfredpersson <alfredpersson.se@gmail.com>
2025-09-24 10:58:02 +0400

    Update structure

--- postchain-eif-contracts/contracts/crc2/NFTBridge.sol
+++ postchain-eif-contracts/contracts/crc2/NFTBridge.sol
@@ -383,26 +383,14 @@ contract NFTBridge is Initializable, PausableUpgradeable, Ownable2StepUpgradeabl
             interfaceId == type(IERC1155Receiver).interfaceId;
     }
 
-    /**
-     * @dev Implementation of {IERC721Receiver-onERC721Received}
-     * Always returns `IERC721Receiver.onERC721Received.selector`
-     */
     function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    /**
-     * @dev Implementation of {IERC1155Receiver-onERC1155Received}
-     * Always returns `IERC1155Receiver.onERC1155Received.selector`
-     */
     function onERC1155Received(address, address, uint256, uint256, bytes calldata) external pure override returns (bytes4) {
         return IERC1155Receiver.onERC1155Received.selector;
     }
 
-    /**
-     * @dev Implementation of {IERC1155Receiver-onERC1155BatchReceived}
-     * Always returns `IERC1155Receiver.onERC1155BatchReceived.selector`
-     */
     function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata) external pure override returns (bytes4) {
         return IERC1155Receiver.onERC1155BatchReceived.selector;
     }
